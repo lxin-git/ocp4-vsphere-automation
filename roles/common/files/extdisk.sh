ad6b107287754d597874b1a16a28360de0419a89
Xin Li <llixinn@cn.ibm.com>
2020-09-09 15:09:34 +0800

    update disk extend script to fit rhel8

--- roles/common/files/extdisk.sh
+++ roles/common/files/extdisk.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-echo -e "n\np\n3\n\n\nt\n3\n8e\nw\n" | fdisk /dev/sda
+lastpart=`fdisk -l /dev/sda|awk 'END {print $1}'`
+lastnumber=${lastpart: -1}
+((newnumber=lastnumber+1))
+echo -e "n\np\n${newnumber}\n\n\nt\n${newnumber}\n8e\nw\n" | fdisk /dev/sda
 partprobe
-kpartx /dev/sda3
-pvcreate /dev/sda3
-vgextend rhel /dev/sda3
+kpartx /dev/sda${newnumber}
+pvcreate /dev/sda${newnumber}
+vgextend rhel /dev/sda${newnumber}
 lvextend -l +100%FREE /dev/rhel/root
 xfs_growfs /dev/mapper/rhel-root
